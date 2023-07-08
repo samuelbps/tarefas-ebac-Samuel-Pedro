1a48916091c0cc818145bb2199fa8d9fb868a569
samuelbps <samuelbpedro1@gmail.com>
2023-07-08 16:00:36 -0300

    PrimeiroProjeto

---
+++ mod5/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package br.com.spedro;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		String ambiente = args[0];
+		if(ambiente.equals("Samuel")) {
+			System.out.println("Hello Samuel");
+		}
+	}
+
+}
