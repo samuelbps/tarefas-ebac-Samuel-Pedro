5da4a3ebea10a5f9d3a94c4dcfc79f8b96eb3e2b
samuelbps <samuelbpedro1@gmail.com>
2023-09-20 16:02:48 -0300

    Exercicio mod 17 modificado

--- generics/Generics/src/App.java
+++ generics/Generics/src/App.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class App {
 
     public static void main(String[] args) {
-       List listaCarros = new ArrayList<Objects>();
+       List<Carro> listaCarros = new ArrayList<Carro>();
 
        Onix onix = new Onix("Onix", 100000.0);
        Civic civic = new Civic("Civic", 120000.0);
